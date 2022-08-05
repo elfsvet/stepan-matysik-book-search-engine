4daca47b5d01362d8038f15b5b32a9209458287b
elfsvet <stepanmatysik@gmail.com>
2022-08-05 16:11:24 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/stepan-matysik-book-search-engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
