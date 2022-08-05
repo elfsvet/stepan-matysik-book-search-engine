142676efa5d5a7e486a830fbc75600a6fc1a053e
elfsvet <stepanmatysik@gmail.com>
2022-08-05 09:46:20 -0400

    added apollo provider

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,83 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation Login($loginEmail2: String!, $loginPassword2: String!) {
+    login(email: $loginEmail2, password: $loginPassword2) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+mutation AddUser($addUserUsername2: String!, $addUserEmail2: String!, $addUserPassword2: String!) {
+    addUser(username: $addUserUsername2, email: $addUserEmail2, password: $addUserPassword2) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+mutation SaveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+mutation RemoveBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
